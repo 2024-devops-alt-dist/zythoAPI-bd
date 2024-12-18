03f528aaccc807a3c55ddc810442cecee1b24d03
Boris Duko <boris.duko@origins-digital.com>
2024-12-18 09:03:47 +0100

    feat: example middleware

---
+++ src/middleware/validators/beerValidator.ts
@@ -0,0 +1,9 @@
+// import { body } from "express-validator";
+
+// export const validateBeerCreation = [
+// 	body("name").notEmpty().trim(),
+// 	body("description").notEmpty(),
+// 	body("abv").isFloat({ min: 0, max: 100 }),
+// 	body("brewery_id").isInt(),
+// 	body("category_id").isInt(),
+// ];
