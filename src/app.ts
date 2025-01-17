cc27757ee84eeee630106abf7a4b60ded4cfcca1
dukoborisorigins <boris.duko@origins-digital.com>
2025-01-17 18:19:51 +0100

    feat: add CORS middleware for localhost:5173

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,32 @@
 // app.ts
 import express, { Application } from "express";
+import cors from "cors";
 import { usersController } from "./controllers/users";
 import beersRoutes from "./routes/beersRoutes";
 import breweriesRoutes from "./routes/breweriesRoutes";
-import swaggerUi from 'swagger-ui-express';
-import { specs } from './swagger/swagger';
+import swaggerUi from "swagger-ui-express";
+import { specs } from "./swagger/swagger";
 
 const app: Application = express();
+
+// CORS Middleware
+app.use(
+	cors({
+		origin: "http://localhost:5173", // frontend URL for Vite app
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 // Middleware
 app.use(express.json());
 
 // Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // test route
 app.get("/", (req, res) => {
-    res.send("Hello Server!");
+	res.send("Hello Server!");
 });
 
 // user route test
